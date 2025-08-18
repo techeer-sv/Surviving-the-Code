2ea66fdbf54c7a4f87ac8398addddd565467dc34
Jungkihong07 <hnn07185@gmail.com>
2025-08-19 04:22:20 +0900

    code linting

--- kihong/week1/2891.py
+++ kihong/week1/2891.py
@@ -1,9 +1,6 @@
 #  2891 카약과 강풍 리뷰
 
 
-import sys
-
-
 def solution(n, s, r, damaged_teams, spare_teams):
     # 각 팀원 초기화
     def init_team(n, s, r):
@@ -41,15 +38,16 @@ def main():
     # 로컬에서 여러 테스트 케이스를 처리하는 방법
     test_cases = [
         # (n, s, r, damaged_teams, spare_teams) 형식으로 테스트 케이스 추가
-        # (5, 2, 1, [2, 4], [3]),
-        # (5, 2, 3, [2, 4], [1, 3, 5]),
+        (5, 2, 1, [2, 4], [3]),
+        (5, 2, 3, [2, 4], [1, 3, 5]),
         (6, 1, 1, [6], [3]),
     ]
 
     for n, s, r, damaged_teams, spare_teams in test_cases:
         result = solution(n, s, r, damaged_teams, spare_teams)
         print(
-            f"Test Case: n={n}, s={s}, r={r}, damaged={damaged_teams}, spare={spare_teams}"
+            f"Test Case: n={n}, s={s}, r={r}"
+            f"damaged={damaged_teams}, spare={spare_teams}"
         )
         print(f"Result: {result}\n")
 
