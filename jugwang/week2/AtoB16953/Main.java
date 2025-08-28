3f7b2fb9059eb3b8dec88b7b72b85814dd586bf8
dhdid <lleewnrhkd@gmail.com>
2025-08-29 03:31:35 +0900

    readme, 주석 추가

--- jugwang/week2/AtoB16953/Main.java
+++ jugwang/week2/AtoB16953/Main.java
@@ -2,21 +2,27 @@ import java.util.Arrays;
 
 public class Main {
     public static int solution(int first, int need) {
-
+        
+        // 연산 횟수
         int answer = 1;
         while (need > first) {
+            // 10으로 나누어 떨어지면 1을 제거
             if ((need%10) == 1){
                 answer++;
                 need /= 10;
+            // 2로 나누어 떨어지면 2로 나누기
             } else if ((need%2) == 0) {
                 answer++;
                 need /= 2;
+            // 위 두 조건을 만족하지 않으면 -1 반환 (불가능한 경우)
             } else {
                 return -1;
             }
         }
+        // B가 A와 같아지면 연산 횟수 반환
         if (first == need) {
             return answer;
+        // 위 두 조건을 만족하지 않으면 -1 반환 (불가능한 경우)
         }else {
             return -1;
         }
