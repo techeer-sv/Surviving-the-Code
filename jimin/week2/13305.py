c30a552e0281064489525ecf0db666649b6fc9c6
Jimin15 <sujean07015@gmail.com>
2025-09-04 17:34:33 +0900

    코드돌린거

--- jimin/week2/13305.py
+++ jimin/week2/13305.py
@@ -1,9 +1,10 @@
-#최소값을 일단 price[0]으로 넣기
-#price리스트의 원소를 하나씩 옮겨가면서
+# 최소값을 일단 price[0]으로 넣기
+# price리스트의 원소를 하나씩 옮겨가면서
 # 원소가 min값보다 클경우 min에다가 해당 거리를 곱해서 result에 넣어주기
-#작은 경우는 min값을 해당 원소로 교체해서 해당 거리르 곱해서 result에 넣어주기
+# 작은 경우는 min값을 해당 원소로 교체해서 해당 거리르 곱해서 result에 넣어주기
 
-#시간복잡도 o(n)
+# 시간복잡도 o(n)
+
 
 def solve(n: int, dist: list, price: list) -> int:
     result = 0
