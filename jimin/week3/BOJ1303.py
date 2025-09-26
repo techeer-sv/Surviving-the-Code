beca738e3831c31d411e8f5f3effb8ca847cd600
Jimin15 <sujean07015@gmail.com>
2025-09-26 20:54:16 +0900

    1303 오류 해결

--- jimin/week3/BOJ1303.py
+++ jimin/week3/BOJ1303.py
@@ -1,4 +1,22 @@
-# 시간 표기법 : O(N × M)
+"""
+board를 차례대로 돌면서, visited를 참고하고, 각 객수를 더하기
+
+for x in 가로:
+    for y in 세로:
+        만약에 방문한 보드가 아니라면
+            dfs 알고리즘을 수행하여 군집 수집
+            수집한 군집에 따라 병력 계산
+
+[시간 복잡도 분석]
+- 외부 이중 for문은 모든 n * m 칸을 한 번씩 순회합니다. (O(nm))
+- DFS는 한 번 호출되면 connected component의 모든 칸을 방문합니다.
+- visited 배열 덕분에 모든 칸은 DFS에 의해 정확히 한 번만 방문됩니다.
+- 따라서, DFS 탐색에 필요한 총 시간은 모든 칸을 방문하는 시간과 동일합니다. (O(nm))
+- 전체 시간 복잡도는 O(nm) + O(nm)으로, O(nm)입니다.
+
+"""
+
+
 def solve(board: list, n, m):
 
     def dfs(board, visited, x, y):
